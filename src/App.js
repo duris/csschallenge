0607d1ca7933891d6d38c8f2d9a03273194e0995
Ross Duris <rossduris@gmail.com>
2023-01-25 18:51:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Editor from './Editor';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const html = `<div class="joyful"></div>`
+  const css = `.joyful{
+    background:#F4FB0A;
+    transform:rotate(45deg);
+    width:100px;
+    height:100px;
+    margin:25px;
+    border-radius:50% 0;
+    box-shadow:2px 2px 3px #ccc;
+  }`
+
+  const initialSrc = (
+    `
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>                
+      </html>
+    `
+  )
+
+ return(
+  <>
+  <h1>Make the Lemon</h1>
+  <main>    
+    <div className='promptPanel'>
+      <div className='promptWall'></div>
+      <Editor htmlStart={html} cssStart={css} initialSrc={initialSrc} />  
+      <div className='quiz'>
+        <div className='joyful'></div>
+      </div>    
     </div>
+    <div className='userPanel'>
+      <Editor htmlStart={''} cssStart={''}/>      
+    </div>
+  </main>
+  </>
   );
 }
-
-export default App;
+export default App
---
+++ src/Editor.js
@@ -0,0 +1,164 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './App.css';
+import AceEditor from "react-ace";
+import "ace-builds";
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-chaos";
+import "ace-builds/src-noconflict/ext-language_tools";
+import "ace-builds/webpack-resolver";
+
+const Editor = ({htmlStart, cssStart, initialSrc}) => {
+
+  const htmlRef = useRef(null)
+  const cssRef = useRef(null)
+  const htmlButton = useRef(null)
+  const cssButton = useRef(null)
+
+
+  const [code, setCode] = React.useState(
+    `.joyful{
+      background:#F4FB0A;
+      transform:rotate(45deg);
+      width:100px;
+      height:100px;
+      margin:25px;
+      border-radius:50% 0;
+      box-shadow:2px 2px 3px #ccc;
+    }`
+  );
+
+  const [answer, setAnswer] = useState(`background:green;`)
+  const [srcDoc, setSrcDoc] = useState(` `);
+  const [initialSrcDoc, setInitialSrcDoc] = useState(` `);
+  const [html, setHtml] = useState(` `)
+  const [css, setCss] = useState(` `)
+  const [quizHtml, setQuizHtml] = useState(` `)
+  const [quizCss, setQuizCss] = useState(` `)
+  const [js, setJs] = useState(` `)
+  
+  function onChange(newValue) {
+    console.log("change", newValue);
+  }
+
+  function handleChange(){
+    // console.log(aceCode)
+  }
+
+  useEffect(() => {
+    setQuizHtml(htmlStart)
+    setQuizCss(cssStart)
+    setInitialSrcDoc(
+      `
+        <html>
+          <body>${htmlStart}</body>
+          <style>${cssStart}</style>                
+        </html>
+      `
+    )
+  },[])
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+    setSrcDoc(
+      `
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>                
+        </html>
+      `
+    )
+    if(css.replace(/\s+/g, "") === `.joyful{
+      background:#F4FB0A;
+      transform:rotate(45deg);
+      width:100px;
+      height:100px;
+      margin:25px;
+      border-radius:50% 0;
+      box-shadow:2px 2px 3px #ccc;
+    }`.replace(/\s+/g, "") && html.replace(/\s+/g, "") === `<div class="joyful"></div>`.replace(/\s+/g, "")){
+      alert('success!');
+    }
+    }, 250)
+  }, [html, css, js])
+
+
+  function toHtml(){
+    const cssDiv = cssRef.current
+    const htmlDiv = htmlRef.current
+
+    cssDiv.style.display = 'none'
+    htmlDiv.style.display = 'block'
+    htmlButton.current.classList.add('active')
+    cssButton.current.classList.remove('active')
+  }
+  function toCss(){ 
+    const cssDiv = cssRef.current
+    const htmlDiv = htmlRef.current
+
+    htmlDiv.style.display = 'none'
+    cssDiv.style.display = 'block'
+    htmlButton.current.classList.remove('active')
+    cssButton.current.classList.add('active')
+  }
+
+  return(
+    <div className='editor-wrap'>
+      <div className='editors'>
+        <div className='tabs'>
+          <button  ref={htmlButton}  className='hmtlButton active' onClick={toHtml}>HTML</button>
+          <button  ref={cssButton}  className='cssButton' onClick={toCss}>CSS</button>
+        </div>
+        <div ref={htmlRef} className='htmlPanel'>
+          <AceEditor
+          className="editor"
+          mode="html"
+          placeholder={quizHtml}          
+          theme="chaos"
+          onChange={(e) => {setHtml(e)}}
+          name="UNIQUE_ID_OF_DIV"
+          height='160px'          
+          width='100%'
+          editorProps={{ $blockScrolling: true }}
+        />
+      </div>
+      <div ref={cssRef} className='cssPanel'>
+        <AceEditor
+          className="editor"
+          mode="css"
+          placeholder={quizCss}
+          theme="chaos"
+          onChange={(e) => setCss(e)}
+          name="UNIQUE_ID_OF_DIV"
+          height='160px'
+          width='100%'            
+          enableBasicAutocompletion='true'    
+          editorProps={{ $blockScrolling: true }}
+        />
+      </div>
+    {/* <AceEditor
+      className="editor"
+      mode="javascript"
+      theme="chaos"
+      onChange={(e) => setJs(e)}
+      name="UNIQUE_ID_OF_DIV"
+      height='100px'
+      width='100%'      
+      editorProps={{ $blockScrolling: true }}
+    /> */}
+    </div>
+    <iframe
+      id="my_iframe"      
+      srcDoc={srcDoc}
+      title="output"
+      sandbox="allow-scripts"
+      frameBorder="1"
+      width="50%"
+      height="200px"
+    />
+    </div>
+  )
+}
+
+export default Editor
